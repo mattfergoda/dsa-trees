644a4a7409af3d202c68e740efa3e95833c0906f
John Flores <johnflores1991@hotmail.com>
2024-01-03 13:24:49 -0600

    countEvens complete

--- tree.js
+++ tree.js
@@ -31,7 +31,20 @@ class TreeNode {
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
   countEvens() {
+    let count = 0;
 
+    let toVisitStack = [this];
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+
+      if (current.val % 2 === 0) count++;
+
+      for (let child of current.children)
+        toVisitStack.push(child);
+    }
+
+    return count;
   }
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
@@ -54,7 +67,7 @@ class Tree {
 
   /** countEvens(): count all nodes in the tree that have even values. */
   countEvens() {
-
+    return !this.root ? 0 : this.root.countEvens();
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
